4ee797d4db6603630acb1f753897b604d3ea99f3
Peak <pglund18@yahoo.com>
2023-09-18 04:55:48 -0500

    webpack throwing confusing error am stumped

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,16 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-
-//call const throughout ^
-
-//manifest file
-
-//serviceworker
-
 // TODO: Add CSS loaders and babel to webpack.
-
-//css and babel
 
 module.exports = () => {
   return {
@@ -27,11 +18,44 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html', //says doesnt exist?
+        title: 'Webpack Plugin',
+      }),
+      new WebpackPwaManifest ({
+        name: 'Text-Editor',
+        short_name: 'PWA',
+        description: 'This is a text editor',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve(__dirname, 'src/images/logo.png' ),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: 'assets/icons',
+          }
+        ]
+      }),
+      new InjectManifest({
+        swSrc: '/src-sw.js', 
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
-      rules: [
+      rules: [{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },},
+      },
         
       ],
     },
